16d634f6c36f4e7b9a4506034a4c2d018dd45b17
UVALDT <learningdesign@virginia.edu>
2023-06-28 16:44:43 -0400

    working on creating calendar

--- AllTheThings.ts
+++ AllTheThings.ts
@@ -1,238 +1,129 @@
 function main(workbook: ExcelScript.Workbook) {
 
-    let cellHasValue: number[][] = [];
-    let cellValue: string[] = [];
-    let colors: string[] = ["b4f2ac", "dfe8ae", "b2bded", "adf0dd"]
-    let colorIndex = 0;
-    let roomHeadings: string[][] = [];
-    let days: string[] = ["M", "T", "W", "Th", "F"]
-    let startOfClass: boolean = false;
-    var classRooms18to30: ExcelScript.Worksheet;
-    var classRooms31to54: ExcelScript.Worksheet;
-    var classRooms55to62: ExcelScript.Worksheet;
-    var classRooms63to99: ExcelScript.Worksheet;
-    var classRoomsTimes: number[][] = [];
-    let startNewCourseBlock: boolean = false;
-    
-    type room = {
-        name: string;
-        capacity: number;
-        schedule: Map<string, string[]>; // e.g. schedule["M"] = monday's schedule (which is times that are filled by class names)
-    }
-    
-    
-        // Your code here
-    
-        console.log("test");
+  let cellHasValue: number[][] = [];
+  let cellValue: string[] = [];
+  let colors: string[] = ["b4f2ac", "dfe8ae", "b2bded", "adf0dd"]
+  let colorIndex = 0;
+  let roomHeadings: string[][] = [];
+  let days: string[] = ["M", "T", "W", "Th", "F"]
+  let startOfClass: boolean = false;
+  var classRooms18to30: ExcelScript.Worksheet;
+  var classRooms31to54: ExcelScript.Worksheet;
+  var classRooms55to62: ExcelScript.Worksheet;
+  var classRooms63to99: ExcelScript.Worksheet;
+  var classRoomsTimes: number[][] = [];
+  let startNewCourseBlock: boolean = false;
+  
+  type room = {
+      name: string;
+      capacity: number;
+      schedule: Map<string, string[]>; // e.g. schedule["M"] = monday's schedule (which is times that are filled by class names)
+  }
+  
+  let rooms: room[] = []
 
-        let NCH107: room = {
-            name: "NCH 107",
-            capacity: 30,
-            schedule: InitializeRoomSchedule()
-        }
+      // Your code here
+  let UNASSIGNEDROOM: room = {
+    name: "Unassigned",
+    capacity: 0,
+    schedule: InitializeRoomSchedule()
+  }
+  
+  let NCH107: room = {
+      name: "NCH 107",
+      capacity: 30,
+      schedule: InitializeRoomSchedule()
+  }
 
-        console.log(NCH107.schedule.get("M"));
-    
-    /*
-        let sheets = workbook.getWorksheets();
-    
-        // Assign specific worksheets based on name
-    
-        for (let i = 0; i < sheets.length; i++) {
-    
-            if (sheets[i].contain("18")) {
-                classRooms18to30 = sheets[i];
-            }
-    
-            if (sheets[i].contain("31")) {
-                classRooms31to54 = sheets[i];
-            }
-    
-            if (sheets[i].contain("55")) {
-                classRooms55to62 = sheets[i];
-            }
-    
-            if (sheets[i].contain("63")) {
-                classRooms63to99 = sheets[i];
-            }
-    
-        }
-    
-        var courses = workbook.getWorksheet("Courses");
-    
-        var classRoomRange = classRooms.getRange("B3:BZ55");
-        classRoomRange.clear();
-        classRoomRange.clear(ExcelScript.ClearApplyTo.formats);
-    
-        var enrollMin: number = classRooms.getRange("B57").getValue() as number;
-        var enrollMax: number = classRooms.getRange("C57").getValue() as number;
-    
-        // Get data
-        type UVAClass =
-            {
-                courseMnemonic: string;
-                courseNumber: string;
-                day: string;
-                enrollment: number;
-                startTime: number;
-                endTime: number;
-                assignedRoom: string;
-                uniqueIndex: number;
-            };
-    
-        let usedRange = courses.getUsedRange();
-        let rowCount = usedRange.getRowCount();
-        let uvaClasses: UVAClass[] = [];
-        let classData = courses.getRange("A4:O" + rowCount as string).getValues();
-    
-        for (let i = 0; i < classData.length; i++) {
-            uvaClasses[i] = {
-                courseMnemonic: classData[i][0] as string,
-                courseNumber: classData[i][1] as string,
-                day: classData[i][5] as string,
-                enrollment: classData[i][9] as number,
-                startTime: classData[i][6] as number,
-                endTime: classData[i][7] as number,
-                assignedRoom: classData[i][12] as string,
-                uniqueIndex: classData[i][14] as number
-            };
-        }
-    
-        classRoomsTimes = classRooms.getRange("A3:A55").getValues() as number[][];
-    
-        roomHeadings = classRooms.getRange("B1:BZ1").getValues() as string[][];
-    
-        for (let i = 0; i < roomHeadings[0].length; i++)
-        {
-          for (let j = 0; j < roomHeadings[0].length; j++)
-          {
-            while (i != j && roomHeadings[0][i] != "")
-            {
-              if (roomHeadings[0][i].trim() == roomHeadings[0][j].trim())
-                throw "You have a duplicate room in the header row 1: " + roomHeadings[0][i];
-              else
-                break; 
-            }
-          }
-        }
-        // Check classes for enrollment and assign to columns in sheet
-        for (let i = 0; i < uvaClasses.length; i++) {
-            if (uvaClasses[i].enrollment <= enrollMax && uvaClasses[i].enrollment >= enrollMin) {
-                CheckClassRoomsTimes(uvaClasses[i].startTime, uvaClasses[i].endTime, uvaClasses[i].courseMnemonic, uvaClasses[i].courseNumber, uvaClasses[i].enrollment, uvaClasses[i].uniqueIndex, uvaClasses[i].assignedRoom, uvaClasses[i].day);
-                colorIndex++; // We use this to change the fill colors on different classes
-                if (colorIndex > colors.length - 1) {
-                    colorIndex = 0;
-                }
-    
-            }
-        }
-    
-    }
-    function CheckClassRoomsTimes(startTime: number, endTime: number, courseMnemonic: string, courseNumber: string, enrollment: number, uniqueIndex: number, assignedRoom: string, day: string) {
-    
-        // First go through to see if any times overlap with existing data
-    
-        let tempUniqueID: number = 0;
-    
-        for (let i = 0; i < classRoomsTimes.length; i++) {
-    
-            // We use the .001 in this if to account for floating point errors
-            if (startTime - .001 <= classRoomsTimes[i][0] && endTime - .001 >= classRoomsTimes[i][0]) {
-    
-                // Check to see if we're working with a new course, for formatting purposes
-                if (uniqueIndex != tempUniqueID)
-                    startNewCourseBlock = true;
-                else startNewCourseBlock = false;
-                tempUniqueID = uniqueIndex;
-    
-                let roomHeadingColumnNumber: number;
-                if (assignedRoom == "")
-                {    
-                  assignedRoom = "Unassigned";
-                }
-                for (let j = 0; j < roomHeadings[0].length; j++) {
-                    if (assignedRoom.trim() == roomHeadings[0][j].trim()) {
-                        roomHeadingColumnNumber = j + 1; // We add one because it is index 0 and starts at column B
-                        for (let j = 0; j < days.length; j++) {
-                            if ((day.includes(days[j]) && day != "Th") ||
-                                (days[j] == "Th" && day == "Th")) {
-                                PopulateDayAtTime(days[j], roomHeadingColumnNumber + j, i + 2, courseMnemonic, courseNumber, enrollment, uniqueIndex); // i + 1 because zero index and it starts at row 2
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    
-    
-    
-    function PopulateDayAtTime(day: string, columnNumber: number, rowNumber: number, courseMnemonic: string, courseNumber: string, enrollment: number, uniqueIndex: number) {
-        let cell = classRooms.getCell(rowNumber, columnNumber);
-    
-        var overlapFound: boolean = false;
-    
-        // Check for overlap
-        for (let j = 0; j < cellHasValue.length; j++) {
-            if (cellHasValue[j][0] == rowNumber && cellHasValue[j][1] == columnNumber) {
-                overlapFound = true;
-                break;
-            }
-            else {
-                overlapFound = false;
-            }
-        }
-    
-        if (overlapFound) {
-            let newCellValue = LookupStoredCellValue(rowNumber, columnNumber, courseMnemonic + " " + courseNumber + ": " + enrollment + " [" + uniqueIndex + "]");
-            cell.setValue(newCellValue);
-            cell.getFormat().getFill().setColor("red");
-        }
-    
-        else {
-            cell.setValue(courseMnemonic + " " + courseNumber + ": " + enrollment + " [" + uniqueIndex + "]");
-            cell.getFormat().getFill().setColor(colors[colorIndex]);
-        }
-    
-        cell.getFormat().getRangeBorder(ExcelScript.BorderIndex.edgeRight).setStyle(ExcelScript.BorderLineStyle.dashDot);
-    
-        if (startNewCourseBlock) {
-            cell.getFormat().getRangeBorder(ExcelScript.BorderIndex.edgeTop).setStyle(ExcelScript.BorderLineStyle.dashDot);
-        }
-    
-    
-    
-        cellHasValue.push([rowNumber, columnNumber]);
-        cellValue.push(courseMnemonic + " " + courseNumber + ": " + enrollment + " [" + uniqueIndex + "]");
-    
-    }
-    
-    function LookupStoredCellValue(x: number, y: number, currentString: string) {
-        for (let i = 0; i < cellValue.length; i++) {
-            if (cellHasValue[i][0] == x && cellHasValue[i][1] == y) {
-                cellValue[i] = cellValue[i] + " / " + currentString;
-                return cellValue[i];
-            }
-        }
-    }
-    */
-    
-    function InitializeRoomSchedule() : Map<string, string[]> {
-        let schedule: Map<string, string[]> = new Map<string, string[]>();
-        let defaultTimes: string[] = [];
+  rooms.push(NCH107);
+
+  let NCH191: room = {
+    name: "NCH 191",
+    capacity: 30,
+    schedule: InitializeRoomSchedule()
+  }
+
+  rooms.push(NCH191);
+
+  let WIL244: room = {
+    name: "Wilson 244",
+    capacity: 30,
+    schedule: InitializeRoomSchedule()
+  }
+
+  rooms.push(WIL244);
+
+  let CHEM206: room = {
+    name: "CHEM 206",
+    capacity: 54,
+    schedule: InitializeRoomSchedule()
+  }
+
+  rooms.push(CHEM206);
+
+  let CHEM204: room = {
+    name: "CHEM 204",
+    capacity: 54,
+    schedule: InitializeRoomSchedule()
+  }
+
+  rooms.push(CHEM204);
         
-        for (let i = 0; i < 53; i++) 
-        {
-            defaultTimes[i] = "";
-        }
-        
-        schedule.set("M", defaultTimes);
-        schedule.set("T", defaultTimes);
-        schedule.set("w", defaultTimes);
-        schedule.set("Th", defaultTimes);
-        schedule.set("F", defaultTimes);
+  rooms.sort( (a,b) => (a.capacity < b.capacity) ? -1 : 1);
 
-        return schedule;
+  
+    
+  var courses = workbook.getWorksheet("Courses");
+  var calendar = workbook.getWorksheet("Calendar");
+  
+  // Get data
+  type UVAClass =
+  {
+      courseMnemonic: string;
+      courseNumber: string;
+      day: string;
+      enrollment: number;
+      startTime: number;
+      endTime: number;
+      assignedRoom: string;
+      uniqueIndex: number;
+  };
+
+  let usedRange = courses.getUsedRange();
+  let rowCount = usedRange.getRowCount();
+  let uvaClasses: UVAClass[] = [];
+  let classData = courses.getRange("A4:O" + rowCount as string).getValues();
+
+  for (let i = 0; i < classData.length; i++) {
+    uvaClasses[i] = {
+        courseMnemonic: classData[i][0] as string,
+        courseNumber: classData[i][1] as string,
+        day: classData[i][5] as string,
+        enrollment: classData[i][9] as number,
+        startTime: classData[i][6] as number,
+        endTime: classData[i][7] as number,
+        assignedRoom: classData[i][12] as string,
+        uniqueIndex: classData[i][14] as number
+    };
+  }
+
+
+  function InitializeRoomSchedule() : Map<string, string[]> {
+    let schedule: Map<string, string[]> = new Map<string, string[]>();
+    let defaultTimes: string[] = [];
+    
+    for (let i = 0; i < 53; i++) 
+    {
+        defaultTimes[i] = "";
     }
+    
+    schedule.set("M", defaultTimes);
+    schedule.set("T", defaultTimes);
+    schedule.set("w", defaultTimes);
+    schedule.set("Th", defaultTimes);
+    schedule.set("F", defaultTimes);
+
+    return schedule;
+  }
     
 }

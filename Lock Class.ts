be4e72d78bd8772d1a745d0c633cef834c251870
jasoben <jasoben@gmail.com>
2023-07-13 17:47:57 -0400

    Update Lock Class.ts

--- Lock Class.ts
+++ Lock Class.ts
@@ -1,5 +1,5 @@
 function main(workbook: ExcelScript.Workbook) {
-    // Your code here
+    // This locks a class on the calendar
     let cell = workbook.getActiveCell();
     let worksheet = workbook.getActiveWorksheet();
     if (worksheet.getName() != "Calendar") {
@@ -8,35 +8,44 @@ function main(workbook: ExcelScript.Workbook) {
     let courses = workbook.getWorksheet("Courses");
     let setRoomName: string = "";
     var roomStartColumn: number;
+    let roomColor: string = "red";
 
-    for (let i = 0; i < 6; i++) {
-      let columnIndex = cell.getColumnIndex();
-      let roomName = worksheet.getCell(0, columnIndex - i).getValue();
-      if (roomName != "") {
-        setRoomName = roomName as string;
-        roomStartColumn = columnIndex - i;
-        break;
+    getRoomNameForCurrentlySelectedClassCell();
+    setRoomInCoursesSheet();
+    colorTheClassOnCalendarSheet();
+
+    function getRoomNameForCurrentlySelectedClassCell() {
+      for (let i = 0; i < 6; i++) {
+        let columnIndex = cell.getColumnIndex();
+        let roomName = worksheet.getCell(0, columnIndex - i).getValue();
+        if (roomName != "") {
+          setRoomName = roomName as string;
+          roomStartColumn = columnIndex - i;
+          break;
+        }
       }
     }
 
-
-    let cellValue = cell.getValue().toString();
-    let rowNumber: number = +cellValue.slice(cellValue.indexOf("{") + 1, cellValue.indexOf("}"));
-    let roomRange = courses.getRange("M1:M1000").getUsedRange();
-    
-    roomRange.getCell(rowNumber - 1, 0).setValue(setRoomName);
-    
-    let rangeToCheck = worksheet.getRangeByIndexes(0, roomStartColumn, 53, 5).getUsedRange();
-    let rowCount = rangeToCheck.getRowCount();
-    let columnCount = rangeToCheck.getColumnCount();
-    let values = rangeToCheck.getValues();
-    for (let i = 0; i < rowCount; i++) {
-      for (let j = 0; j < columnCount; j++) {
-        let value = values[i][j] as string;
-        if (value.includes("{" + rowNumber.toString() + "}")) {
-          rangeToCheck.getCell(i,j).getFormat().getFill().setColor("red");
-        }
-      }
+    function setRoomInCoursesSheet() {
+      let cellValue = cell.getValue().toString();
+      let rowNumber: number = +cellValue.slice(cellValue.indexOf("{") + 1, cellValue.indexOf("}")); // The row is listed on the Calendar sheet between {}
+      let roomRange = courses.getRange("M1:M1000").getUsedRange();
       
-    } 
+      roomRange.getCell(rowNumber - 1, 0).setValue(setRoomName);
+    }
+    
+    function colorTheClassOnCalendarSheet() {
+      let rangeToCheck = worksheet.getRangeByIndexes(0, roomStartColumn, 53, 5).getUsedRange();
+      let rowCount = rangeToCheck.getRowCount();
+      let columnCount = rangeToCheck.getColumnCount();
+      let values = rangeToCheck.getValues();
+      for (let i = 0; i < rowCount; i++) {
+        for (let j = 0; j < columnCount; j++) {
+          let value = values[i][j] as string;
+          if (value.includes("{" + rowNumber.toString() + "}")) {
+            rangeToCheck.getCell(i,j).getFormat().getFill().setColor(roomColor);
+          }
+        }
+      } 
+    }
 }
